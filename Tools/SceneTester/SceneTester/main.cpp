d5ea832401c9ca5b167e37f98505b26fd4a1bfcd
Mike Murrell <michaeljm116@gmail.com>
2019-08-15 13:56:17 -0400

    idk

--- Tools/SceneTester/SceneTester/main.cpp
+++ Tools/SceneTester/SceneTester/main.cpp
@@ -1,11 +1,12 @@
-#include "sceneLoader.hpp"
+//#include "sceneLoader.hpp"
 #include "kdtree.hpp"
 //../../../Assets/Levels/
 
 int main() {
 	std::string dir = "../../../Assets/Levels/";
 	glm::vec3 bob = glm::vec3(1);
-	std::vector<Bounds> transforms = LoadScene(dir, "Pong/Arena");
-
+	std::vector<Bounds> transforms = SceneLoader::LoadScene(dir, "Pong/Arena");
+	KdTree Bob;
+	Bob.build(transforms, 4);
 	return 0;
 }
--- Tools/SceneTester/SceneTester/sceneLoader.hpp
+++ Tools/SceneTester/SceneTester/sceneLoader.hpp
@@ -7,87 +7,89 @@
 
 static const int COMPONENT_TRANSFORM = 0x02;
 
-using namespace tinyxml2;
-std::vector<TransformComponent*> loadNodes(tinyxml2::XMLElement* start, tinyxml2::XMLElement* finish) {
-	std::vector<TransformComponent*> ret;
-	bool lastOne = false;
-	// node
-	while (!lastOne) {
 
-		const char* name;
-		bool hasChildren;
-		int flags;
-		int tags;
-		bool dynamic;
+namespace SceneLoader {
+	std::vector<TransformComponent*> loadNodes(tinyxml2::XMLElement* start, tinyxml2::XMLElement* finish) {
+		std::vector<TransformComponent*> ret;
+		bool lastOne = false;
+		// node
+		while (!lastOne) {
 
-		start->QueryStringAttribute("Name", &name);
-		start->QueryBoolAttribute("hasChildren", &hasChildren);
-		start->QueryIntAttribute("Flags", &flags);
-		start->QueryIntAttribute("Tags", &tags);
-		start->QueryBoolAttribute("Dynamic", &dynamic);
+			const char* name;
+			bool hasChildren;
+			int flags;
+			int tags;
+			bool dynamic;
 
-		if (flags & COMPONENT_TRANSFORM) {
-			glm::vec3 pos;
-			glm::vec3 rot;
-			glm::vec3 sca;
+			start->QueryStringAttribute("Name", &name);
+			start->QueryBoolAttribute("hasChildren", &hasChildren);
+			start->QueryIntAttribute("Flags", &flags);
+			start->QueryIntAttribute("Tags", &tags);
+			start->QueryBoolAttribute("Dynamic", &dynamic);
 
-			XMLElement* transform = start->FirstChildElement("Transform");
+			if (flags & COMPONENT_TRANSFORM) {
+				glm::vec3 pos;
+				glm::vec3 rot;
+				glm::vec3 sca;
 
-			XMLElement* position = transform->FirstChildElement("Position");
-			position->QueryFloatAttribute("x", &pos.x);
-			position->QueryFloatAttribute("y", &pos.y);
-			position->QueryFloatAttribute("z", &pos.z);
+				tinyxml2::XMLElement* transform = start->FirstChildElement("Transform");
 
-			XMLElement* rotation = transform->FirstChildElement("Rotation");
-			rotation->QueryFloatAttribute("x", &rot.x);
-			rotation->QueryFloatAttribute("y", &rot.y);
-			rotation->QueryFloatAttribute("z", &rot.z);
+				tinyxml2::XMLElement* position = transform->FirstChildElement("Position");
+				position->QueryFloatAttribute("x", &pos.x);
+				position->QueryFloatAttribute("y", &pos.y);
+				position->QueryFloatAttribute("z", &pos.z);
 
-			XMLElement* scale = transform->FirstChildElement("Scale");
-			scale->QueryFloatAttribute("x", &sca.x);
-			scale->QueryFloatAttribute("y", &sca.y);
-			scale->QueryFloatAttribute("z", &sca.z);
+				tinyxml2::XMLElement* rotation = transform->FirstChildElement("Rotation");
+				rotation->QueryFloatAttribute("x", &rot.x);
+				rotation->QueryFloatAttribute("y", &rot.y);
+				rotation->QueryFloatAttribute("z", &rot.z);
 
-			TransformComponent* trans = new TransformComponent(pos, rot, sca);
+				tinyxml2::XMLElement* scale = transform->FirstChildElement("Scale");
+				scale->QueryFloatAttribute("x", &sca.x);
+				scale->QueryFloatAttribute("y", &sca.y);
+				scale->QueryFloatAttribute("z", &sca.z);
 
-			ret.push_back(trans);
+				TransformComponent* trans = new TransformComponent(pos, rot, sca);
+
+				ret.push_back(trans);
+			}
+
+			////////////////// HAS CHILDREN ///////////////////
+			if (hasChildren) {
+				std::vector<TransformComponent*> children = loadNodes(start->FirstChildElement("Node"), start->LastChildElement("Node"));
+				for (auto c : children)
+					ret.push_back(c);
+			}
+			///////////////////////////////////////////////////
+
+			//loop it up;
+			if (start != finish)
+				start = start->NextSiblingElement();
+			else
+				lastOne = true;
 		}
-
-		////////////////// HAS CHILDREN ///////////////////
-		if (hasChildren) {
-			std::vector<TransformComponent*> children = loadNodes(start->FirstChildElement("Node"), start->LastChildElement("Node"));
-			for (auto c : children)
-				ret.push_back(c);
-		}
-		///////////////////////////////////////////////////
-
-		//loop it up;
-		if (start != finish)
-			start = start->NextSiblingElement();
-		else
-			lastOne = true;
+		return ret;
 	}
-	return ret;
-}
 
-std::vector<Bounds> LoadScene(std::string dir, std::string name)
-{
-	XMLDocument doc;
-	XMLError eResult = doc.LoadFile((dir + name + ".xml").c_str());
-	XMLNode * pNode = doc.FirstChild();
+	std::vector<Bounds> LoadScene(std::string dir, std::string name)
+	{
+		tinyxml2::XMLDocument doc;
+		tinyxml2::XMLError eResult = doc.LoadFile((dir + name + ".xml").c_str());
+		tinyxml2::XMLNode * pNode = doc.FirstChild();
 
-	XMLElement* pRoot = doc.FirstChildElement("Root");// ->FirstChildElement("Scene");
-	XMLElement* sceneN = pRoot->FirstChildElement("Scene");
-	int sceneNumber;
-	sceneN->QueryIntAttribute("Num", &sceneNumber);
+		tinyxml2::XMLElement* pRoot = doc.FirstChildElement("Root");// ->FirstChildElement("Scene");
+		tinyxml2::XMLElement* sceneN = pRoot->FirstChildElement("Scene");
+		int sceneNumber;
+		sceneN->QueryIntAttribute("Num", &sceneNumber);
 
-	XMLElement* first = pRoot->FirstChildElement("Node");
-	XMLElement* last = pRoot->LastChildElement("Node");
+		tinyxml2::XMLElement* first = pRoot->FirstChildElement("Node");
+		tinyxml2::XMLElement* last = pRoot->LastChildElement("Node");
 
-	std::vector<TransformComponent*> ret = loadNodes(first, last);
-	std::vector<Bounds> bounds;
-	bounds.reserve(ret.size());
-	for (auto b : ret)
-		bounds.emplace_back(b->toBounds());
-	return bounds;
+		std::vector<TransformComponent*> ret = loadNodes(first, last);
+		std::vector<Bounds> Bounds;
+		Bounds.reserve(ret.size());
+		for (auto b : ret)
+			Bounds.emplace_back(b->toBounds());
+		return Bounds;
+	}
 }
