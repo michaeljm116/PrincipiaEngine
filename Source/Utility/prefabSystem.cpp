0e109fdf61b3a1fc760db9666d67fccac68e5b39
Mike Murrell <michaeljm116@gmail.com>
2020-02-18 06:34:13 -0500

    serialize-node

--- Source/Utility/prefabSystem.cpp
+++ Source/Utility/prefabSystem.cpp
@@ -1,56 +1,74 @@
 #include "prefabSystem.h"
+#include "serialize-node.h"
 
-Principia::PrefabSystem::PrefabSystem()
-{
-	addComponentType<PrefabComponent>();
-	addComponentType<NodeComponent>();
-}
+#ifndef XMLCheckResult
+#define XMLCheckResult(a_eResult) if (a_eResult != tinyxml2::XML_SUCCESS) { printf("Error: %i\n", a_eResult); return a_eResult; }
+#endif
 
-Principia::PrefabSystem::~PrefabSystem()
-{
-}
+using namespace tinyxml2;
 
-void Principia::PrefabSystem::initialize()
-{
-	nodeMapper.init(*world);
-	prefabMapper.init(*world);
-}
 
-void Principia::PrefabSystem::added(artemis::Entity & e)
-{
-	PrefabComponent* pc = prefabMapper.get(e);
-	NodeComponent* nc = nodeMapper.get(e);
-	if (pc->save) 
+namespace Principia {
+	PrefabSystem::PrefabSystem()
 	{
-
+		addComponentType<PrefabComponent>();
+		addComponentType<NodeComponent>();
 	}
-	else 
+
+	PrefabSystem::~PrefabSystem()
 	{
+	}
 
+	void PrefabSystem::initialize()
+	{
+		nodeMapper.init(*world);
+		prefabMapper.init(*world);
+	}
+
+	void PrefabSystem::added(artemis::Entity & e)
+	{
+		PrefabComponent* pc = prefabMapper.get(e);
+		NodeComponent* nc = nodeMapper.get(e);
+		if (pc->save)
+		{
+
+		}
+		else
+		{
+
+		}
+	}
+
+	void PrefabSystem::processEntity(artemis::Entity & e)
+	{
+	}
+
+	tinyxml2::XMLError PrefabSystem::SavePrefab(std::string prefab, NodeComponent * node)
+	{
+		//Create the XML Document
+		XMLDocument doc;
+
+		//Create root of the tree
+		XMLNode* pRoot = doc.NewElement("Root");
+		doc.InsertFirstChild(pRoot);
+
+		//Insert all the relevant data
+		XMLElement * element = SERIALIZENODE.saveNode(node, &doc);
+		pRoot->InsertEndChild(element);
+
+		//Save the file and check for any errors
+		XMLError eResult = doc.SaveFile((prefab + ".prefab").c_str());
+		XMLCheckResult(eResult);
+
+		//return that sumbish
+		return eResult;
+	}
+
+	tinyxml2::XMLError PrefabSystem::LoadPrefab(std::string prefab, NodeComponent * node)
+	{
+		XMLDocument doc;
+		XMLError eResult = doc.LoadFile((prefab).c_str());
+
+		return eResult;
 	}
 }
-
-void Principia::PrefabSystem::processEntity(artemis::Entity & e)
-{
-}
-
-//tinyxml2::XMLError Principia::PrefabSystem::SavePrefab(std::string prefab, NodeComponent * node)
-//{
-//	XMLDocument doc;
-//	XMLError eResult = doc.SaveFile((prefab).c_str());
-//
-//	XMLNode* pRoot = doc.NewElement("Root");
-//	doc.InsertFirstChild(pRoot);
-//
-//	//XMLElement* element = 
-//
-//	return eResult;
-//}
-//
-//tinyxml2::XMLError Principia::PrefabSystem::LoadPrefab(std::string prefab, NodeComponent * node)
-//{
-//	XMLDocument doc;
-//	XMLError eResult = doc.LoadFile((prefab).c_str());
-//
-//	return eResult;
-//}
